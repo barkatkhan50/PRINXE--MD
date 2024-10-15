4670e8807f482e3735a54e4922b29f9a0c80b360
barkatkhan50 <103475269+barkatkhan50@users.noreply.github.com>
2024-10-15 12:19:46 +0500

    Update config.js

--- config.js
+++ config.js
@@ -26,8 +26,8 @@ for (let i = 0; i < ownerlist.length; i += 2) {
 //💌------------------------------------------💌
 
 
-//💌global.pairingNumber = "" //put your bot number here💌
-global.mods = ['923042205427'] 
+//💌global.pairingNumber = "923012707501" //put your bot number here💌
+global.mods = ['923012707501'] 
 global.prems = ['']
 global.allowed = ['']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
